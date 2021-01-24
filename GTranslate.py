bebdd8a7902e206cef4537ecadbc7832e54ad0c5
Leon <letconex@gmail.com>
2021-01-24 18:20:24 +0200

    Create GTranslate.py

---
+++ GTranslate.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+# sourceLang = "de"
+# targetLang = "ro"
+sourceLang = st.text_input("Enter source language:")
+targetLang = st.text_input("Enter target language:")
+sourceText = st.text_input("Enter text to translate:")
+url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + sourceText
+response = requests.get(url)
+if st.button('Translate'):
+  result = response.text
+  indexx = result.index('","')
+  result = result[4:int(indexx)]
+  # st.write(result)
+  st.success(result)
+
+#import datetime as dt
+#st.write(f"Now is {now}")
